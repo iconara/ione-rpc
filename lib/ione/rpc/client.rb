31116af6a66475f51c5ffb34ea526c3bfb6d0c8b
Theo <theo@iconara.net>
2014-04-25 11:54:21 +0200

    Add API docs to Client

--- lib/ione/rpc/client.rb
+++ lib/ione/rpc/client.rb
@@ -2,6 +2,18 @@
 
 module Ione
   module Rpc
+    # This is the base class of client peers.
+    #
+    # You can either create a subclass and add your own high-level convenience
+    # methods for constructing and sending your custom requests, or you can
+    # create a standalone client object and call {#send_request}.
+    #
+    # A subclass may optionally implement {#initialize_connection} to send a
+    # message immediately on a successful connection, and {#choose_connection}
+    # to decide which connection to use for a request.
+    #
+    # The client will handle connections to multiple server peers, and
+    # automatically reconnect to them when they disconnect.
     class Client
       def initialize(hosts, codec, options={})
         @hosts = hosts
@@ -15,19 +27,56 @@ module Ione
         @connections = []
       end
 
+      # A client is connected when it has at least one open connection.
       def connected?
         @lock.synchronize { @connections.any? }
       end
 
+      # Start the client and connect to all hosts. This also starts the IO
+      # reactor if it was not already started.
+      #
+      # The returned future resolves when all hosts have been connected to, and
+      # if one or more fails to connect the client will periodically try again,
+      # and the future will not resolve until all of them have connected.
+      #
+      # @return [Ione::Future<Ione::Rpc::Client>] a future that resolves to the
+      #   client when all hosts have been connected to.
       def start
         @io_reactor.start.flat_map { connect_all }.map(self)
       end
 
+      # Stop the client and close all connections. This also stops the IO
+      # reactor if it has not already stopped.
+      #
+      # @return [Ione::Future<Ione::Rpc::Client>] a future that resolves to the
+      #   client when all connections have closed and the IO reactor has stopped.
       def stop
         @lock.synchronize { @connections = [] }
         @io_reactor.stop.map(self)
       end
 
+      # Send a request to a server peer. The peer chosen is determined by the
+      # Implementation of {#choose_connection}, which is random selection by
+      # default.
+      #
+      # If a connection closes between the point where it was chosen and when
+      # the message was written to it, the request is retried on another
+      # connection. For all other errors the request is not retried and it is
+      # up to the caller to determine if the request is safe to retry.
+      #
+      # If a logger has been specified the following will be logged:
+      # * A warning when a connection has closed and the request will be retried
+      # * A warning when a request fails for another reason
+      # * A warning when there are no open connections
+      #
+      # @param [Object] request the request to send.
+      # @param [Object] connection the connection to send the request on. This
+      #   parameter is internal and should only be used from {#initialize_connection}.
+      # @return [Ione::Future<Object>] a future that resolves to the response
+      #   from the server, or fails because there was an error while processing
+      #   the request (this is not the same thing as the server sending an
+      #   error response – that is protocol specific and up to the implementation
+      #   to handle), or when there was no connection open.
       def send_request(request, connection=nil)
         connection = connection || @lock.synchronize { choose_connection(@connections, request) }
         if connection
@@ -52,10 +101,43 @@ module Ione
 
       protected
 
+      # Override this method to send a request when a connection has been
+      # established, but before the future returned by {#start} resolves.
+      #
+      # It's important that if you need to send a special message to initialize
+      # a connection that you send it to the right connection. To do this pass
+      # the connection as second argument to {#send_request}, see the example
+      # below.
+      #
+      # @example Sending a startup request
+      #   def initialize_connection(connection)
+      #     send_request(MyStartupRequest.new, connection)
+      #   end
+      #
+      # @return [Ione::Future] a future that resolves when the initialization
+      #   is complete. If this future fails the connection fails.
       def initialize_connection(connection)
         Future.resolved
       end
 
+      # Override this method to implement custom request routing strategies.
+      # Before a request is encoded and sent over a connection this method will
+      # be called with all available connections and the request object (i.e.
+      # the object passed to {#send_request}).
+      #
+      # The default implementation picks a random connection.
+      #
+      # The connection objects have a `#host` property that use if you want to
+      # do routing based on host.
+      #
+      # @example Routing messages consistently based on a property of the request
+      #   def choose_connection(connections, request)
+      #     connections[request.some_property.hash % connections.size]
+      #   end
+      #
+      # @param [Array<Object>] connections all the open connections.
+      # @param [Object] request the request to be sent.
+      # @return [Object] the connection that should receive the request.
       def choose_connection(connections, request)
         connections.sample
       end
