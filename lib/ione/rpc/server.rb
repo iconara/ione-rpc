0115a52e9b784d4083f7de90320200472f99d29f
Theo <theo@iconara.net>
2014-04-26 13:24:41 +0200

    Add docs for Server

--- lib/ione/rpc/server.rb
+++ lib/ione/rpc/server.rb
@@ -2,6 +2,11 @@
 
 module Ione
   module Rpc
+    # This is the base class of server peers.
+    #
+    # To implement a server you need to create a subclass of this class and
+    # implement {#handle_message}. You can also optionally implement
+    # {#handle_connection} to do initialization when a new client connects.
     class Server
       attr_reader :port
 
@@ -14,20 +19,49 @@ module Ione
         @bind_address = options[:bind_address] || '0.0.0.0'
       end
 
+      # Start listening for client connections. This also starts the IO reactor
+      # if it was not already started.
+      #
+      # The returned future resolves when the server is ready to accept
+      # connections, or fails if there is an error starting the server.
+      #
+      # @return [Ione::Future<Ione::Rpc::Server>] a future that resolves to the
+      #   server when all hosts have been connected to.
       def start
         @io_reactor.start.flat_map { setup_server }.map(self)
       end
 
+      # Stop the server and close all connections. This also stops the IO reactor
+      # if it has not already stopped.
+      #
+      # @return [Ione::Future<Ione::Rpc::Server>] a future that resolves to the
+      #   server when all connections have closed and the IO reactor has stopped.
       def stop
         @io_reactor.stop.map(self)
       end
 
       protected
 
-      def handle_connection(peer)
+      # Override this method to do work when a new client connects.
+      #
+      # This method may be called concurrently.
+      #
+      # @return [nil] the return value of this method is ignored
+      def handle_connection(connection)
       end
 
-      def handle_message(message, peer)
+      # Override this method to handle requests.
+      #
+      # You must respond to all requests, otherwise the client will eventually
+      # use up all of its channels and not be able to send any more requests.
+      #
+      # This method may be called concurrently.
+      #
+      # @param [Object] message a (decoded) message from a client
+      # @param [#host, #port, #on_closed] connection the client connection that
+      #   received the message
+      # @return [Ione::Future<Object>] a future that will resolve to the response.
+      def handle_message(message, connection)
         Future.resolved
       end
 
@@ -41,6 +75,7 @@ module Ione
         end
       end
 
+      # @private
       class ServerPeer < Peer
         def initialize(connection, codec, server)
           super(connection, codec)
